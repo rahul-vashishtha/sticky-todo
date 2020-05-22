1d6a2e168cfafc82f7144707c9b4e1752166fe13
TeckFreak <rahulvashistha96@gmail.com>
2020-05-23 01:31:32 +0530

    Added Item addition functionality.

--- src/components/ToDoItem.ts
+++ src/components/ToDoItem.ts
@@ -6,13 +6,74 @@
  */
 
 import * as UI from "@nodegui/nodegui";
+import { Utils } from "../helpers/Utils";
 
 export class ToDoItem extends UI.QWidget {
-    constructor() {
+    private onItemDeleted?: () => void;
+
+    constructor(private text: string) {
         super();
+
+        this.initializeComponents();
     }
 
     private initializeComponents() {
+        const layout = new UI.QBoxLayout(UI.Direction.LeftToRight);
+        this.setLayout(layout);
+        this.setMinimumSize(290, 50);
+        this.setMaximumSize(1920, 50);
+        this.setInlineStyle(`
+            border-radius: 6px;
+            background: white;
+            border: 2px outset #EEEEEE;
+            margin: 5px;
+            padding-left: 10px;
+            padding-right: 10px;
+        `);
 
+        const checkbox = new UI.QCheckBox();
+        checkbox.setText(this.text);
+        checkbox.addEventListener("toggled", (checked) => {
+            if (checked) {
+                checkbox.setInlineStyle(`
+                    color: #999;
+                    text-decoration: line-through;
+                    font-style: italic;
+                `);
+            }
+            else {
+                checkbox.setInlineStyle(`
+                    color: #1B1B1B;
+                    text-decoration: '';
+                    font-style: normal;
+                `);
+            }
+        });
+
+        const btnDelete = new UI.QLabel();
+        btnDelete.setText("X");
+        btnDelete.setCursor(UI.CursorShape.PointingHandCursor);
+        btnDelete.addEventListener(UI.WidgetEventTypes.MouseButtonRelease, () => {
+            this.onItemDeleted?.call(this);
+        });
+
+        this.addEventListener(UI.WidgetEventTypes.MouseButtonDblClick, () => {
+            Utils.getInputFromDialog("Edit Task", "Enter the updated task", (input) => {
+                if (!Utils.isEmptyorWhitespace(input)) {
+                    checkbox.setText(input);
+                }
+            });
+        });
+
+        layout.addSpacing(10);
+        layout.addWidget(checkbox);
+        layout.addSpacing(10);
+        layout.addStretch();
+        layout.addWidget(btnDelete);
+        layout.addSpacing(10);
+    }
+
+    public onDeleteClicked(callback: () => void): void {
+        this.onItemDeleted = callback;
     }
 }
--- src/views/MainWindow.ts
+++ src/views/MainWindow.ts
@@ -8,8 +8,12 @@
 import UI = require("@nodegui/nodegui");
 import fs = require("fs");
 import path = require("path");
+import { Utils } from "../helpers/Utils";
+import { ToDoItem } from "../components/ToDoItem";
 
 export class MainWindow extends UI.QMainWindow {
+    private todoListWidget!: UI.QWidget;
+
     constructor() {
         super();
 
@@ -22,9 +26,10 @@ export class MainWindow extends UI.QMainWindow {
      */
     protected initializeComponents() {
         this.setWindowTitle("Sticky Todo");
-        this.setMinimumSize(250, 300);
+        this.setMinimumSize(300, 300);
         this.setStyleSheet(fs.readFileSync(path.resolve(__dirname, "../assets/styles/style.css"), "utf8"));
         this.setWindowIcon(new UI.QIcon(path.resolve(__dirname, "../assets/images/logo.png")));
+        this.setWindowFlag(UI.WindowType.WindowMaximizeButtonHint, false);
 
         const centralWidget = new UI.QWidget();
         centralWidget.setInlineStyle(`
@@ -37,30 +42,52 @@ export class MainWindow extends UI.QMainWindow {
         this.setCentralWidget(centralWidget);
 
         const headerWidget = new UI.QWidget();
-        headerWidget.setMinimumSize(250, 40);
+        headerWidget.setMinimumSize(300, 40);
         headerWidget.setMaximumSize(1920, 40);
         headerWidget.setLayout(new UI.QBoxLayout(UI.Direction.RightToLeft));
         headerWidget.layout?.setContentsMargins(0, 0, 0, 0);
+        headerWidget.setInlineStyle(`margin-top: 5px;`)
+
+        this.todoListWidget = new UI.QWidget();
+        this.todoListWidget.setInlineStyle(`
+            flex-direction: column;
+        `);
+        this.todoListWidget.setLayout(new UI.FlexLayout());
 
         const addBtn = new UI.QPushButton();
-        addBtn.setText("+");
-        addBtn.setMinimumSize(30, 30);
-        addBtn.setMaximumSize(30, 30);
+        addBtn.setText("+ Add New Item");
+        addBtn.setMinimumSize(150, 35);
+        addBtn.setMaximumSize(150, 35);
         addBtn.setCursor(UI.CursorShape.PointingHandCursor);
-        addBtn.addEventListener("clicked", (checked) => {
+        addBtn.setInlineStyle(`
+            font-size: 13px;
+            font-weight: 300;
+            border-radius: 4px;
+        `);
+        addBtn.addEventListener("clicked", this.addToDoItem.bind(this));
 
-        })
-
+        (headerWidget.layout as UI.QBoxLayout).addStretch();
         headerWidget.layout?.addWidget(addBtn);
         (headerWidget.layout as UI.QBoxLayout).addStretch();
 
-        const todoListWidget = new UI.QWidget();
-        todoListWidget.setInlineStyle(`
-            flex-direction: column;
-        `);
-        todoListWidget.setLayout(new UI.FlexLayout());
-
         centralWidget.layout?.addWidget(headerWidget, 0, 0);
-        centralWidget.layout?.addWidget(todoListWidget, 1, 0);
+        centralWidget.layout?.addWidget(this.todoListWidget, 1, 0);
+    }
+
+    private addToDoItem(): void {
+        Utils.getInputFromDialog("Enter item", "Enter the item to add", (input) => {
+
+            if (!Utils.isEmptyorWhitespace(input)) {
+
+                const item = new ToDoItem(input);
+                item.onDeleteClicked(() => {
+                    console.log("delete called");
+                    this.todoListWidget.layout!.removeWidget(item);
+                    this.todoListWidget.update();
+                });
+
+                this.todoListWidget.layout?.addWidget(item);
+            }
+        });
     }
 }
