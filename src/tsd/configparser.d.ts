889f75f18331e2ba4adecf272f9e45d336bbea26
TeckFreak <rahulvashistha96@gmail.com>
2020-07-03 02:19:28 +0530

    Added declaration for configparser

---
+++ src/tsd/configparser.d.ts
@@ -0,0 +1,8 @@
+/*
+ * File: configparser.d.ts
+ * Project: sticky-todo
+ * File Created: Thursday, 2nd July 2020 5:26:49 pm
+ * Copyright : © 2020 Rinnegan Technologies Pvt. Ltd.
+ */
+
+declare module 'configparser';
